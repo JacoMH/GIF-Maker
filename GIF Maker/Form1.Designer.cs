fe731a74a596bbb34d13e45fefcfe231318ab1a4
Jacob Heyes <heyesjacob@gmail.com>
2024-10-21 13:52:13 +0100

    Add project files.

---
+++ GIF Maker/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace GIF_Maker
+{
+    partial class GIFMaker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImageOpenBtn = new System.Windows.Forms.Button();
+            this.imageList = new System.Windows.Forms.ListBox();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.imageUpBtn = new System.Windows.Forms.Button();
+            this.imageDownBtn = new System.Windows.Forms.Button();
+            this.imageDisplayBox = new System.Windows.Forms.PictureBox();
+            this.createGIFBtn = new System.Windows.Forms.Button();
+            this.deleteAllImages = new System.Windows.Forms.Button();
+            this.framePreviewLbl = new System.Windows.Forms.Label();
+            this.ReplaceImageBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageDisplayBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImageOpenBtn
+            // 
+            this.ImageOpenBtn.Location = new System.Drawing.Point(32, 65);
+            this.ImageOpenBtn.Name = "ImageOpenBtn";
+            this.ImageOpenBtn.Size = new System.Drawing.Size(175, 65);
+            this.ImageOpenBtn.TabIndex = 0;
+            this.ImageOpenBtn.Text = "Open Images";
+            this.ImageOpenBtn.UseVisualStyleBackColor = true;
+            this.ImageOpenBtn.Click += new System.EventHandler(this.ImageOpenBtn_Click);
+            // 
+            // imageList
+            // 
+            this.imageList.FormattingEnabled = true;
+            this.imageList.ItemHeight = 20;
+            this.imageList.Location = new System.Drawing.Point(248, 21);
+            this.imageList.Name = "imageList";
+            this.imageList.Size = new System.Drawing.Size(352, 304);
+            this.imageList.TabIndex = 1;
+            this.imageList.SelectedIndexChanged += new System.EventHandler(this.imageList_SelectedIndexChanged);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(208, 350);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(151, 55);
+            this.deleteBtn.TabIndex = 2;
+            this.deleteBtn.Text = "delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // imageUpBtn
+            // 
+            this.imageUpBtn.Location = new System.Drawing.Point(391, 350);
+            this.imageUpBtn.Name = "imageUpBtn";
+            this.imageUpBtn.Size = new System.Drawing.Size(86, 55);
+            this.imageUpBtn.TabIndex = 3;
+            this.imageUpBtn.Text = "^";
+            this.imageUpBtn.UseVisualStyleBackColor = true;
+            this.imageUpBtn.Click += new System.EventHandler(this.imageUpBtn_Click);
+            // 
+            // imageDownBtn
+            // 
+            this.imageDownBtn.Location = new System.Drawing.Point(503, 350);
+            this.imageDownBtn.Name = "imageDownBtn";
+            this.imageDownBtn.Size = new System.Drawing.Size(86, 55);
+            this.imageDownBtn.TabIndex = 4;
+            this.imageDownBtn.Text = "v";
+            this.imageDownBtn.UseVisualStyleBackColor = true;
+            this.imageDownBtn.Click += new System.EventHandler(this.imageDownBtn_Click);
+            // 
+            // imageDisplayBox
+            // 
+            this.imageDisplayBox.Location = new System.Drawing.Point(669, 126);
+            this.imageDisplayBox.Name = "imageDisplayBox";
+            this.imageDisplayBox.Size = new System.Drawing.Size(247, 114);
+            this.imageDisplayBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageDisplayBox.TabIndex = 5;
+            this.imageDisplayBox.TabStop = false;
+            // 
+            // createGIFBtn
+            // 
+            this.createGIFBtn.Location = new System.Drawing.Point(780, 449);
+            this.createGIFBtn.Name = "createGIFBtn";
+            this.createGIFBtn.Size = new System.Drawing.Size(175, 54);
+            this.createGIFBtn.TabIndex = 6;
+            this.createGIFBtn.Text = "Create GIF";
+            this.createGIFBtn.UseVisualStyleBackColor = true;
+            this.createGIFBtn.Click += new System.EventHandler(this.createGIFBtn_Click);
+            // 
+            // deleteAllImages
+            // 
+            this.deleteAllImages.Location = new System.Drawing.Point(208, 420);
+            this.deleteAllImages.Name = "deleteAllImages";
+            this.deleteAllImages.Size = new System.Drawing.Size(151, 55);
+            this.deleteAllImages.TabIndex = 7;
+            this.deleteAllImages.Text = "delete all";
+            this.deleteAllImages.UseVisualStyleBackColor = true;
+            this.deleteAllImages.Click += new System.EventHandler(this.deleteAllImages_Click);
+            // 
+            // framePreviewLbl
+            // 
+            this.framePreviewLbl.AutoSize = true;
+            this.framePreviewLbl.Location = new System.Drawing.Point(734, 87);
+            this.framePreviewLbl.Name = "framePreviewLbl";
+            this.framePreviewLbl.Size = new System.Drawing.Size(117, 20);
+            this.framePreviewLbl.TabIndex = 8;
+            this.framePreviewLbl.Text = "Frame Preview:";
+            // 
+            // ReplaceImageBtn
+            // 
+            this.ReplaceImageBtn.Location = new System.Drawing.Point(391, 420);
+            this.ReplaceImageBtn.Name = "ReplaceImageBtn";
+            this.ReplaceImageBtn.Size = new System.Drawing.Size(151, 55);
+            this.ReplaceImageBtn.TabIndex = 10;
+            this.ReplaceImageBtn.Text = "Replace Image";
+            this.ReplaceImageBtn.UseVisualStyleBackColor = true;
+            this.ReplaceImageBtn.Click += new System.EventHandler(this.ReplaceImageBtn_Click);
+            // 
+            // GIFMaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 515);
+            this.Controls.Add(this.ReplaceImageBtn);
+            this.Controls.Add(this.framePreviewLbl);
+            this.Controls.Add(this.deleteAllImages);
+            this.Controls.Add(this.createGIFBtn);
+            this.Controls.Add(this.imageDisplayBox);
+            this.Controls.Add(this.imageDownBtn);
+            this.Controls.Add(this.imageUpBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.imageList);
+            this.Controls.Add(this.ImageOpenBtn);
+            this.Name = "GIFMaker";
+            this.Text = "GIF Maker";
+            ((System.ComponentModel.ISupportInitialize)(this.imageDisplayBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ImageOpenBtn;
+        private System.Windows.Forms.ListBox imageList;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button imageUpBtn;
+        private System.Windows.Forms.Button imageDownBtn;
+        private System.Windows.Forms.PictureBox imageDisplayBox;
+        private System.Windows.Forms.Button createGIFBtn;
+        private System.Windows.Forms.Button deleteAllImages;
+        private System.Windows.Forms.Label framePreviewLbl;
+        private System.Windows.Forms.Button ReplaceImageBtn;
+    }
+}
+
---
+++ GIF Maker/Form1.cs
@@ -0,0 +1,275 @@
+﻿using ImageMagick;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace GIF_Maker
+{
+    public partial class GIFMaker : Form
+    {
+        public GIFMaker()
+        {
+            InitializeComponent();
+        }
+
+
+        //class for images
+        class newGIF
+        {
+            public string name;
+            public string path;
+        }
+        List<newGIF> GifMaker = new List<newGIF>(); //create a list of the newGIF class
+        private void ImageOpenBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // open file dialog
+                OpenFileDialog open = new OpenFileDialog();
+                // image filters
+                open.Filter = "Image Files(*.jpg; *.jpeg; *.png; *.bmp)|*.jpg; *.jpeg; *.png; *.bmp";
+                open.Multiselect = true;
+
+
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string file in open.FileNames)
+                    {
+                        MessageBox.Show(file);
+
+                        //new image object
+                        newGIF images = new newGIF();
+                        images.name = Path.GetFileName(file);
+                        images.path = file;
+
+                        //add to final array
+                        GifMaker.Add(images);
+                    }
+
+                    refreshListBox(GifMaker);
+                }
+            }
+        }
+
+
+        private void refreshListBox(List<newGIF> GifMaker)
+        {
+            //delete all items in listbox
+            imageList.Items.Clear();
+
+            foreach (var item in GifMaker)
+            {
+                imageList.Items.Add(item.name);
+            }
+            imageList.Refresh();
+        }
+
+        private void imageList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //search Gif objects for corresponding image name, which is linked to the path of the image, this is then displayed
+            int index = GifMaker.FindIndex(x => x.name == imageList.SelectedItem.ToString());
+
+            //use index to find image
+            if (index != -1)
+            {
+                displayImage(GifMaker[index].path);
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+
+        }
+
+        private void displayImage(string path)
+        {
+            imageDisplayBox.ImageLocation = path;
+        }
+
+        private void imageUpBtn_Click(object sender, EventArgs e)
+        {
+            int CurrentIndex = GifMaker.FindIndex(x => x.name == imageList.SelectedItem.ToString());
+            int ReplacedIndex = CurrentIndex - 1;
+            if ((CurrentIndex - 1) != -1)
+            {
+                MoveImage(CurrentIndex, ReplacedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Already at the back.");
+            }
+        }
+
+        private void imageDownBtn_Click(object sender, EventArgs e)
+        {
+            int CurrentIndex = GifMaker.FindIndex(x => x.name == imageList.SelectedItem.ToString());
+            int ReplacedIndex = CurrentIndex + 1;
+            if ((CurrentIndex + 1) > (imageList.Items.Count - 1) && imageList.SelectedIndex != -1)
+            {
+                MessageBox.Show("Already at the front.");
+            }
+            else
+            {
+                MoveImage(CurrentIndex, ReplacedIndex);
+            }
+        }
+
+        private void MoveImage(int CurrentIndex, int ReplacedIndex)
+        {
+            if (imageList.SelectedIndex != -1)
+            {
+                string CurrentIndexTempName = GifMaker[CurrentIndex].name;
+                string CurrentIndexTempPath = GifMaker[CurrentIndex].path;
+
+
+                string ReplacedIndexTempName = GifMaker[ReplacedIndex].name;
+                string ReplacedIndexTempPath = GifMaker[ReplacedIndex].path;
+
+
+                //flip them
+                GifMaker[ReplacedIndex].name = CurrentIndexTempName;
+                GifMaker[ReplacedIndex].path = CurrentIndexTempPath;
+
+                GifMaker[CurrentIndex].name = ReplacedIndexTempName;
+                GifMaker[CurrentIndex].path = ReplacedIndexTempPath;
+
+                refreshListBox(GifMaker);
+
+                //keep the original image selected
+                imageList.SelectedIndex = ReplacedIndex;
+            }
+
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            if (imageList.SelectedIndex != -1)
+            {
+                int index = GifMaker.FindIndex(x => x.name == imageList.SelectedItem.ToString());
+
+                //removes object in list at the index selected and updates listbox
+                GifMaker.RemoveAt(index);
+                refreshListBox(GifMaker);
+            }
+        }
+
+        private void deleteAllImages_Click(object sender, EventArgs e)
+        {
+            //removes all objects in list and updates the listbox
+            GifMaker.Clear();
+            refreshListBox(GifMaker);
+        }
+
+        private void CreateGif()
+        {
+            //may have to convert images into bmp and use some sort of encoder?? or maybe have to use a library on top
+            using (MagickImageCollection collection = new MagickImageCollection())
+            {
+                foreach (var item in GifMaker)
+                {
+                    MagickImage image = new MagickImage(item.path);
+
+                    //per frame
+                    image.AnimationDelay = 50; //milliseconds maybe include option to customise the speed of the animation
+                    image.Resize(1920, 1080);
+                    collection.Add(image);
+                }
+                //optimise
+                collection.OptimizeTransparency();
+                //      collection.Optimize();
+
+                //save the gif
+                collection.Write(@"C:\Users\user\Desktop\output\output.gif", MagickFormat.Gif);
+
+                //create window for file name, location of save etc.
+            }
+            MessageBox.Show("Gif created successfully");
+        }
+
+        private void createGIFBtn_Click(object sender, EventArgs e)
+        {
+            CreateGif();
+        }
+
+        private void OpenVideoBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // open file dialog
+                OpenFileDialog open = new OpenFileDialog();
+                // image filters
+                open.Filter = "Video files |*.wmv; *.3g2; *.3gp; *.3gp2; *.3gpp; *.amv; *.asf;  *.avi; *.bin; *.cue; *.divx; *.dv; *.flv; *.gxf; *.iso; *.m1v; *.m2v; *.m2t; *.m2ts; *.m4v; " +
+                          " *.mkv; *.mov; *.mp2; *.mp2v; *.mp4; *.mp4v; *.mpa; *.mpe; *.mpeg; *.mpeg1; *.mpeg2; *.mpeg4; *.mpg; *.mpv2; *.mts; *.nsv; *.nuv; *.ogg; *.ogm; *.ogv; *.ogx; *.ps; *.rec; *.rm; *.rmvb; *.tod; *.ts; *.tts; *.vob; *.vro; *.webm; *.dat; ";
+                open.Multiselect = true;
+
+
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    using (var videoFrames = new MagickImageCollection(open.FileName.ToString()))
+                    {
+                        foreach (var frame in videoFrames)
+                        {
+                            MessageBox.Show(frame.ToString());
+                        }
+                    }
+                    foreach (string file in open.FileNames)
+                    {
+                        MessageBox.Show(file);
+
+                        //new image object
+                        newGIF images = new newGIF();
+                        images.name = Path.GetFileName(file);
+                        images.path = file;
+
+                        //add to final array
+                        GifMaker.Add(images);
+                    }
+
+                    refreshListBox(GifMaker);
+                }
+            }
+        }
+
+        private void ReplaceImageBtn_Click(object sender, EventArgs e)
+        {
+            if (imageList.SelectedItem != null)
+            {
+                int index = GifMaker.FindIndex(x => x.name == imageList.SelectedItem.ToString());
+                GifMaker.RemoveAt(index);
+
+                // open file dialog
+                OpenFileDialog open = new OpenFileDialog();
+                // image filters
+                open.Filter = "Image Files(*.jpg; *.jpeg; *.png; *.bmp)|*.jpg; *.jpeg; *.png; *.bmp";
+
+
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    MessageBox.Show(open.FileName);
+
+                    //new image object
+                    newGIF images = new newGIF();
+                    images.name = Path.GetFileName(open.ToString());
+                    images.path = open.FileName;
+
+                    //add to final array
+                    GifMaker.Insert(index, images);
+
+                    refreshListBox(GifMaker);
+                }
+            }
+        }
+            
+
+    }
+}
